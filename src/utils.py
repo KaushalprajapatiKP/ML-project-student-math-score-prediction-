14d6592dfeaa979133952c0dbb3e73679e83e3cc
kaushal prajapati <kaushalprajapati5296@gmail.com>
2024-05-23 12:26:59 +0530

    created front end using flask and prediction pipeline
    created prediction pipeline which accepts details from users and give results of math score prediction using flask frontend

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os, sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 
 from src.exception import CustomException
 
@@ -44,4 +45,14 @@ def evaluate_models(X_train, y_train,X_test, y_test, models, params):
         
     except Exception as e:
         raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e, sys)
+
     
